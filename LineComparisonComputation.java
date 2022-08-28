0513e7d6324531c3d24d282678c64906d873d044
drs317lakshami <devrajkumar317@gmail.com>
2022-08-28 14:48:11 +0530

    Adding:length Of Line as a Point

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -6,8 +6,33 @@ public class LineComparisonComputation {
 
 	public static void main(String args[])
     {
-               System.out.println("Welcome to Line Comparison Computation Program");
+		int x1,x2,y1,y2;
+
+        double length;
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter x1 point");
+
+        x1=sc.nextInt();
+
+        System.out.println("enter y1 point");
+
+        y1=sc.nextInt();
+
+        System.out.println("enter x2point");	
+
+        x2=sc.nextInt();
+
+        System.out.println("enter y2 point");
+
+        y2=sc.nextInt();
+
+        length=Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+
+        System.out.println("Length of Lines is : "+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+length);
+
 
     }
 
-}
+    }

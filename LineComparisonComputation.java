0748014f586bb2ee11fc7f238edeffdd842f2ffb
drs317lakshami <devrajkumar317@gmail.com>
2022-08-28 13:32:10 +0530

    Welcome To Line Comparison Program

---
+++ LineComparisonComputation.java
@@ -0,0 +1,13 @@
+package bridgelabz.com;
+
+import java.util.Scanner;
+
+public class LineComparisonComputation {
+
+	public static void main(String args[])
+    {
+               System.out.println("Welcome to Line Comparison Computation Program");
+
+    }
+
+}
